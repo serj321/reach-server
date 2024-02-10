bd142bdcfa46ad54bf30ad210773e7b2f2deb97e
serj321 <77907685+serj321@users.noreply.github.com>
2024-02-10 18:54:00 -0500

    start project

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/serj321/reach-backend
+
+go 1.21.6
+
+require (
+	github.com/coreos/go-oidc/v3 v3.9.0
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/net v0.20.0
+	golang.org/x/oauth2 v0.16.0
+)
+
+require (
+	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/joho/godotenv"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
+
+func init() {
+    if err := godotenv.Load(); err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+}
+
+func randString(nByte int) (string, error){
+	b := make([]byte, nByte)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
+
+func setCallbackCookie(w http.ResponseWriter, r *http.Request, name, value string){
+	c := &http.Cookie{
+		Name: name,
+		Value: value,
+		MaxAge: int(time.Hour.Seconds()),
+		Secure: r.TLS != nil,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, c)
+}
+
+func main() {
+	ctx := context.Background()
+
+	clientID := os.Getenv("GOOGLE_CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+
+	provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
+	if err != nil{
+		log.Fatal(err)
+	}
+	config := oauth2.Config{
+		ClientID: clientID,
+		ClientSecret: clientSecret,
+		Endpoint: provider.Endpoint(),
+		RedirectURL:  "http://localhost:8080/auth/google/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("endpoint reached with method: %v\n", r.Method)
+		state, err := randString(16)
+		if err != nil{
+			http.Error(w, "Internal error when generating random string.", http.StatusInternalServerError)
+			return
+		}
+		setCallbackCookie(w, r, "state", state)
+
+		http.Redirect(w, r, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/google/callback", func(w http.ResponseWriter, r *http.Request){
+		log.Printf("endpoint callback reached with method: %v\n", r.Method)
+		state, err := r.Cookie("state")
+		println(state)
+		if err != nil{
+			http.Error(w, "state was not found", http.StatusBadRequest)
+			return
+		}
+		if r.URL.Query().Get("state") != state.Value {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil{
+			http.Error(w, "failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		userInfo, err := provider.UserInfo(ctx, oauth2.StaticTokenSource(oauth2Token))
+		if err != nil{
+			http.Error(w, "Failed to get user info: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resp := struct {
+			OAuth2Token *oauth2.Token
+		 	UserInfo    *oidc.UserInfo 
+		}{oauth2Token, userInfo}
+		data, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil{
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Write(data)
+	})
+	log.Printf("listening on http://%s/", "0.0.0.0:8080")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
